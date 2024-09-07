cba4379debd3e0c5e6be2a11c255050eadf56508
Joe Reynolds <j7f7r7@gmail.com>
2024-09-07 18:34:32 -0500

    SUCCESS! Dockerize!

--- cookbooks/cookbooks/settings.py
+++ cookbooks/cookbooks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,23 +76,36 @@ WSGI_APPLICATION = 'cookbooks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'cookbook',
-#         'USER': 'cookbookuser',
-#         'PASSWORD': 'password',
-#         'HOST': 'localhost', # set in docker-compose.yml
-#         'PORT': '', # default postgres port
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# for connecting to postgres db in AWS RDS
+# DATABASES = {
+#     'aws': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_USER_PWD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': 5432,
+#     }
+# }
+
+# for connecting to local postgres db on Joe's machine (Thelio, system76, Ubuntu 22.04)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost', # set in docker-compose.yml
+        'PORT': '5432', # default postgres port
+    }
+}
 
 
 # Password validation
